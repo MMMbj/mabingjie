e953360d6b6a94905ffcbe7ff827a0b374428ace
MMMbj <1030027168@qq.com>
2024-11-09 18:00:44 +0800

    Update main.c

--- main.c
+++ main.c
@@ -2,17 +2,29 @@
 //1030027168@qq.com
 //马冰洁
 #include <stdio.h>
-
+long long factorial(int n);
 int main() {
-
-    int arr[] = { 10, 20, 30, 40, 50 };
-
-    int length = sizeof(arr) / sizeof(arr[0]);
-
-    printf("length: %d\n", length);
-    for (int i = 0; i < length; i++) {
-        printf("Element at index %d: %d\n", i, arr[i]);
+    int number;
+    scanf("%d", &number);
+    if (number < 0) {
+        printf("阶乘未定义于负整数。\n");
+    }
+    else {
+        printf("%d 的阶乘是 %lld\n", number, factorial(number));
     }
 
     return 0;
+}
+
+long long factorial(int n) {
+    if (n == 0 || n == 1) {
+        return 1; 
+    }
+
+    long long result = 1;
+    for (int i = 2; i <= n; i++) {
+        result *= i; 
+    }
+
+    return result;
 }
