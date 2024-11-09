3f9b89a8081913439b76871d44fea28ec35f0b9a
MMMbj <1030027168@qq.com>
2024-11-09 17:42:48 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,18 @@
-//
-//
-//
+//202411716230
+//1030027168@qq.com
+//马冰洁
 #include <stdio.h>
-int main()
-{
-    printf("Hello World");
+
+int main() {
+
+    int arr[] = { 10, 20, 30, 40, 50 };
+
+    int length = sizeof(arr) / sizeof(arr[0]);
+
+    printf("length: %d\n", length);
+    for (int i = 0; i < length; i++) {
+        printf("Element at index %d: %d\n", i, arr[i]);
+    }
+
     return 0;
 }
