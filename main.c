f3c2ec30919e1164b1984580a792bfb4dd01bab3
MMMbj <1030027168@qq.com>
2024-11-09 21:18:08 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,30 +1,17 @@
 //202411716230
 //1030027168@qq.com
 //马冰洁
-#include <stdio.h>
-long long factorial(int n);
+#include<stdio.h>
 int main() {
-    int number;
-    scanf("%d", &number);
-    if (number < 0) {
-        printf("阶乘未定义于负整数。\n");
-    }
-    else {
-        printf("%d 的阶乘是 %lld\n", number, factorial(number));
-    }
-
-    return 0;
-}
-
-long long factorial(int n) {
-    if (n == 0 || n == 1) {
-        return 1; 
-    }
-
-    long long result = 1;
-    for (int i = 2; i <= n; i++) {
-        result *= i; 
-    }
-
-    return result;
+	int n, i, m;
+	unsigned long long factorial;
+	scanf_s("%d", &n);
+	for (i = 1;i <= n;i++) {
+		factorial = 1;
+		for (m = 1;m <= i;m++) {
+			factorial *= m;
+		}
+	}
+	printf("%llu\n",factorial);
+	return 0;
 }
